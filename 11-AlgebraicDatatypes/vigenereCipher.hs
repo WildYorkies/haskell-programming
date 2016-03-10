e16bb6619b7bb7303299a8c5e559895ece390c9f
Cale <cale.short@gmail.com>
2016-03-10 14:20:46 -0500

    consolidate code
    put alphaList, numList, shift, unshift under their functions, remove unnecessary imports

--- 11-AlgebraicDatatypes/vigenereCipher.hs
+++ 11-AlgebraicDatatypes/vigenereCipher.hs
@@ -2,26 +2,24 @@
 module VigenereCipher where
 
 import Data.List (cycle)
-import Data.Char (ord, chr, toUpper, isAlpha)
+import Data.Char (toUpper)
 import Data.Maybe (fromJust)
 
 phrase = "MEET AT DAWN"
 keyword = "ALLY"
 cipherTest = "MPPR AE OYWY"
 
-alphaList :: [(Char, Int)]
-alphaList = zip ['A'..'Z'] [0..25]
-
-numList :: [(Int, Char)]
-numList = zip [0..25] ['A'..'Z']
-
 findNum :: Char -> Int
 findNum x = fromJust $ lookup x alphaList
+  where alphaList :: [(Char, Int)]
+        alphaList = zip ['A'..'Z'] [0..25]
 
 findLetter :: Int -> Char
 findLetter x | x > 25    = fromJust $ lookup (mod x 26) numList
              | x < 0     = fromJust $ lookup (length numList + x) numList
-             | otherwise = fromJust $ lookup x numList 
+             | otherwise = fromJust $ lookup x numList
+  where numList :: [(Int, Char)]
+        numList = zip [0..25] ['A'..'Z']
 
 cipherPhrase :: String -> String -> [(Char, Char)]
 cipherPhrase secret keyword = go (map toUpper secret) (cycle (map toUpper keyword))
@@ -29,16 +27,14 @@ cipherPhrase secret keyword = go (map toUpper secret) (cycle (map toUpper keywor
         go (' ':ps) (k:ks) = (' ', ' ') : go ps (k:ks)
         go (p:ps)   (k:ks) = (p, k)     : go ps ks
 
-shiftChar :: (Char, Char) -> Char
-shiftChar (' ', ' ') = ' '
-shiftChar (x, y) = findLetter ((findNum x) + (findNum y))
-
-unShiftChar :: (Char, Char) -> Char
-unShiftChar (' ', ' ') = ' '
-unShiftChar (x, y) = findLetter ((findNum x) - (findNum y))
-
 vigenere :: String -> String -> String
 vigenere secret keyword = map shiftChar $ cipherPhrase secret keyword
+  where shiftChar :: (Char, Char) -> Char
+        shiftChar (' ', ' ') = ' '
+        shiftChar (x, y)     = findLetter ((findNum x) + (findNum y))
 
 unVigenere :: String -> String -> String
 unVigenere scramble keyword = map unShiftChar $ cipherPhrase scramble keyword
+  where unShiftChar :: (Char, Char) -> Char
+        unShiftChar (' ', ' ') = ' '
+        unShiftChar (x, y)     = findLetter ((findNum x) - (findNum y))
