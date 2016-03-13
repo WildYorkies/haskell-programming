59e439cf0a504f7320b600acff01e30723654364
Cale Short <cale.short@gmail.com>
2016-03-13 14:52:53 -0400

    refactor to reduce lines

--- 11-AlgebraicDatatypes/vigenereCipher.hs
+++ 11-AlgebraicDatatypes/vigenereCipher.hs
@@ -1,13 +1,11 @@
 -- vigenereCipher.hs
-module VigenereCipher where
+module VigenereCipher (vigenere) where
 
 import Data.List (cycle)
 import Data.Char (toUpper)
 import Data.Maybe (fromJust)
 
-phrase = "MEET AT DAWN"
-keyword = "ALLY"
-cipherTest = "MPPR AE OYWY"
+data Operation = Hide | Reveal
 
 findNum :: Char -> Int
 findNum x = fromJust $ lookup x alphaList
@@ -21,20 +19,16 @@ findLetter x | x > 25    = fromJust $ lookup (mod x 26) numList
   where numList :: [(Int, Char)]
         numList = zip [0..25] ['A'..'Z']
 
-cipherPhrase :: String -> String -> [(Char, Char)]
-cipherPhrase secret keyword = go (map toUpper secret) (cycle (map toUpper keyword))
+createCipher :: String -> String -> [(Char, Char)]
+createCipher phrase keyword = go (map toUpper phrase) (cycle (map toUpper keyword))
   where go []       (k:ks) = []
         go (' ':ps) (k:ks) = (' ', ' ') : go ps (k:ks)
         go (p:ps)   (k:ks) = (p, k)     : go ps ks
 
-vigenere :: String -> String -> String
-vigenere secret keyword = map shiftChar $ cipherPhrase secret keyword
-  where shiftChar :: (Char, Char) -> Char
-        shiftChar (' ', ' ') = ' '
-        shiftChar (x, y)     = findLetter ((findNum x) + (findNum y))
+shiftChar :: Operation -> (Char, Char) -> Char
+shiftChar _  (' ', ' ') = ' '
+shiftChar Hide   (x, y) = findLetter ((findNum x) + (findNum y))
+shiftChar Reveal (x, y) = findLetter ((findNum x) - (findNum y))
 
-unVigenere :: String -> String -> String
-unVigenere scramble keyword = map unShiftChar $ cipherPhrase scramble keyword
-  where unShiftChar :: (Char, Char) -> Char
-        unShiftChar (' ', ' ') = ' '
-        unShiftChar (x, y)     = findLetter ((findNum x) - (findNum y))
+vigenere :: String -> String -> Operation -> String
+vigenere phrase keyword op = map (shiftChar op) $ createCipher phrase keyword
